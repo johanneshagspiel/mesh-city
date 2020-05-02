4465aa5917e5a64d406328baa942117185eafccf
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-02 21:35:00 +0200

    Added linting & testing infrastructure

--- setup.py
+++ setup.py
@@ -1,15 +1,41 @@
-from setuptools import setup, find_packages
+import subprocess
+from abc import ABC
+from distutils.cmd import Command
+from typing import List
+
 import pipfile
+from pipfile import Pipfile
+from setuptools import find_packages, setup
 
 
-def get_pipfile_dependencies(category: str):
-	project_pipfile = pipfile.load('Pipfile')
-	dependencies = []
+def get_pipfile_dependencies(category: str) -> List[str]:
+	project_pipfile: Pipfile = pipfile.load('Pipfile')
+	dependencies: List[str] = []
 	for name, version in project_pipfile.data[category].items():
 		if version == '*':
 			version = ''
 		dependencies.append(name + version)
 	return dependencies
+
+
+class CommandAdapter(Command, ABC):
+	user_options = []
+
+	def initialize_options(self) -> None:
+		pass
+
+	def finalize_options(self) -> None:
+		pass
+
+
+class PylintCommand(CommandAdapter):
+	def run(self) -> None:
+		subprocess.check_call(args='pylint capacity', cwd='src', shell=True)
+
+
+class UnittestCommand(CommandAdapter):
+	def run(self) -> None:
+		subprocess.check_call(args='python -m unittest discover', cwd='src', shell=True)
 
 
 setup(
@@ -21,4 +47,8 @@ setup(
 	python_requires='>=3.8',
 	install_requires=get_pipfile_dependencies('default'),
 	extras_require={'dev': get_pipfile_dependencies('develop')},
+	cmdclass={
+		'lint': PylintCommand,
+		'test': UnittestCommand,
+	},
 )
