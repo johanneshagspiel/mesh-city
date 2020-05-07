edf14e479b3ca13590cf96a3fde4c91ce03f8a72
Borna Salarian <B.Salarian@student.tudelft.nl>
2020-05-07 21:33:08 +0200

    Added the ability to store user information (key and quota) in a text file with JSON format

--- src/mesh_city/util/google_maps/google_api_util.py
+++ src/mesh_city/util/google_maps/google_api_util.py
@@ -1,51 +1,79 @@
 import os
+import json
 from pathlib import Path
+
 
 class google_api_util:
 	temp_path = Path(__file__).parents[2]
-	api_file_path = Path.joinpath(temp_path, 'resources','api_key.txt')
+	api_file_path = Path.joinpath(temp_path, 'resources', 'api_key.txt')
+
 
 	def __init__(self):
-		self.quota = input("Please enter your quota\n")
+		#self.quota = input("Please enter your quota\n")
 		self.usage = 0
-
-		if((self.get_api_key()) == -1):
+		# If it's the first time that the user is entering their key and quota
+		if (self.get_api_key()) == -1:
 			self.api_key = input("Please enter your api-key\n")
-			self.store_api_Key(self.api_key)
+			self.quota = input("Please enter your quota\n")
+			self.store_key_and_quota(self.api_key, self.quota)
 
 		else:
 			self.api_key = self.get_api_key()
 
-	def store_api_Key(self, api_key):
-		with open(self.api_file_path, 'w') as storage:
-			storage.write(api_key)
-			storage.close()
+	def store_key_and_quota(self, api_key , init_quota):
+		with open(self.api_file_path, 'w') as storage_json:
+			user_info = {
+				"api_key" : api_key,
+				"quota" : init_quota
+			}
+			storage_json.write(json.dumps(user_info))
+			storage_json.close()
+
+
 
 	def get_api_key(self):
-		if((self.check_key_exist() & self.check_file_exist()) == False):
+		if (self.check_key_exist() & self.check_file_exist()) == False:
 			print("There is no apy-key stored")
 			return -1
 		with open(self.api_file_path, 'r') as storage:
-			return storage.readline()
+			#return storage.readline()
+			user_info = json.loads(storage)
+			return user_info["api_key"]
 
 	def check_file_exist(self):
-		if(os.path.exists(self.api_file_path) == False):
+		if os.path.exists(self.api_file_path) == False:
 			print("api-key.txt has been deleted - new file will be created")
 			open(self.api_file_path, "x")
 			return True
 		return True
 
 	def check_key_exist(self):
-		if(os.path.getsize(self.api_file_path) == 0):
+		if os.path.getsize(self.api_file_path) == 0:
 			return False
 		return True
 
 	def check_usage_against_quota(self, oldUsage):
 		quota = int(self.quota)
-		if(quota - oldUsage) <= 100:
+		if (quota - oldUsage) <= 100:
 			print("Warning, you are getting close to your quota limit!")
 
 	def increase_usage(self):
 		old_usage = self.usage
 		self.check_usage_against_quota(old_usage)
 		self.usage = old_usage + 1
+
+	def get_quota(self):
+		with open(self.api_file_path, 'r') as storage:
+			# return storage.readline()
+			user_info = json.loads(storage)
+			return user_info["quota"]
+
+
+
+def main():
+	test = google_api_util()
+	test.check_key_exist()
+
+
+if __name__ == "__main__":
+	main()
