c4cfafc0d7b266d2e305cc9b3f55a8ad59b562a9
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-05-07 14:45:05 +0200

    Changed default value

--- src/mesh_city/main.py
+++ src/mesh_city/main.py
@@ -4,6 +4,6 @@ from .util.google_maps import google_api_util, google_maps_entity
 test = google_api_util.google_api_util()
 test2 = google_maps_entity.google_maps_entity(test)
 
-test2.get_and_store_location(47.503029,9.754461)
+test2.get_and_store_location(40, 40)
 
 print('Running main...')
