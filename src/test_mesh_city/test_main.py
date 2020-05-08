aa8dffb8aa9c04c4a7d49494b8e5d0ea76b31c57
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-08 15:17:33 +0200

    Adjusted test formatting rules

--- src/test_mesh_city/test_main.py
+++ src/test_mesh_city/test_main.py
@@ -1,4 +1,4 @@
-# pylint: disable=C,R
+# pylint: disable=C0114,R0201
 
 import unittest
 
@@ -6,5 +6,8 @@ from mesh_city.main import print_start_info
 
 
 class MainTestCase(unittest.TestCase):
+	""" An example test case for main """
+
 	def test_main(self):
+		""" A (useless) example test for the splash message """
 		print_start_info()
