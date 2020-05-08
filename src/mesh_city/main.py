62bc6ba49bb9593fcdedff324051e2175c18063d
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-08 20:06:07 +0200

    Post-merge fixes

--- src/mesh_city/main.py
+++ src/mesh_city/main.py
@@ -1,23 +1,19 @@
 """ Entry point of the application """
+
 from mesh_city.util.google_maps.google_api_util import google_api_util
 from mesh_city.util.google_maps.google_maps_entity import google_maps_entity
 from mesh_city.gui.self_made_map import self_made_map
 
-#test = google_api_util()
-#test2 = google_maps_entity(test)
 
-#test2.load_images_map(51.923699, 5.492631)
-#test2.load_images_map(51.998875, 4.373495)
+def main() -> None:
+	# test = google_api_util()
+	# test2 = google_maps_entity(test)
 
-test = self_made_map()
-test
+	# test2.load_images_map(51.923699, 5.492631)
+	# test2.load_images_map(51.998875, 4.373495)
 
-
-def print_start_info() -> None:
-	""" Print a small splash message """
-	print("Welcome to Mesh City")
-	print("--------------------")
+	test = self_made_map()
 
 
 if __name__ == "__main__":
-	print_start_info()
+	main()
