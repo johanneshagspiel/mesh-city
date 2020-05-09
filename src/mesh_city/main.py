8455e6b9f3489c3b97a9058ca184030ef8f04b52
wfvmaas <wfvmaas@gmail.com>
2020-05-09 16:51:57 +0200

    Fixed some merge errors

--- src/mesh_city/main.py
+++ src/mesh_city/main.py
@@ -1,7 +1,7 @@
 """ Entry point of the application """
 
 from mesh_city.util.google_maps.google_api_util import GoogleApiUtil
-
+from mesh_city.util.google_maps.google_maps_entity import GoogleMapsEntity
 
 def print_start_info() -> None:
 	""" Print a small splash message """
@@ -12,13 +12,10 @@ def print_start_info() -> None:
 def main() -> None:
 	print_start_info()
 
-	test = GoogleApiUtil()
-	test.check_key_exist()
+	google_api_util = GoogleApiUtil()
+	google_api_util.check_key_exist()
 
-	graaf_florisstraat_test_coordinates_box = (51.918164, 4.45569, 51.921415, 4.459962)
 
-	test = google_maps_entity(api_util)
-	test.get_area(51.918164, 4.45569, 51.921415, 4.459962, 20, 640)
 
 
 if __name__ == "__main__":
