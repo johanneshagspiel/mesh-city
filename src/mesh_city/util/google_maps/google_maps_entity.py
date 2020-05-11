d0488a38b03beee78482e8d22a6e738b56945052
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-05-11 13:14:11 +0200

    Added cropping of google maps images

--- src/mesh_city/util/google_maps/google_maps_entity.py
+++ src/mesh_city/util/google_maps/google_maps_entity.py
@@ -1,6 +1,8 @@
 import googlemaps
 import math
 from pathlib import Path
+from PIL import Image
+import requests
 
 class google_maps_entity:
 	temp_path = Path(__file__).parents[2]
@@ -12,12 +14,15 @@ class google_maps_entity:
 		self.client = googlemaps.Client(key=self.google_api_util.get_api_key())
 
 	def get_and_store_location(self, x, y, name):
-		size = (640, 640)
-		center = (x, y)
-		zoom = 20
-		scale = 2
-		format = "png"
+		x = str(x)
+		y = str(y)
+		zoom = str(20)
+		width = str(640)
+		height = str(640)
+		scale = str(2)
+		format = "PNG"
 		maptype = "satellite"
+
 		language = None
 		region = None
 		markers = None
@@ -25,17 +30,30 @@ class google_maps_entity:
 		visible = None
 		style = None
 
-		#filename = str(self.request_number) + "_" + str(x) + "_" + str(y) + ".png"
+		response = requests.get("https://maps.googleapis.com/maps/api/staticmap?" +
+		                        "center=" + x + "," + y + "&zoom=" + zoom +
+		                        "&size=" + width + "x" + height + "&scale=" + scale +
+		                        "&format=" + format + "&maptype=" + maptype +
+		                        "&key=" + self.google_api_util.get_api_key())
+
+		# filename = str(self.request_number) + "_" + str(x) + "_" + str(y) + ".png"
 		filename = name
 		to_store = Path.joinpath(self.images_folder_path, filename)
 
-		f = open(to_store, 'wb')
-		for chunk in googlemaps.maps.static_map(self.client, size, center, zoom, scale,
-		                                        format, maptype, language, region,
-		                                        markers, path, visible, style):
-			if chunk:
-				f.write(chunk)
-		f.close()
+		with open(to_store, 'wb') as output:
+			_ = output.write(response.content)
+
+		get_image = Image.open(to_store)
+		left = 40
+		top = 40
+		right = 1240
+		bottom = 1240
+
+		filename = name
+		to_store = Path.joinpath(self.images_folder_path, filename)
+
+		im1 = get_image.crop(box=(left, top, right, bottom))
+		im1.save(fp=to_store)
 
 		self.google_api_util.increase_usage()
 		#self.increase_request_number()
@@ -71,10 +89,10 @@ class google_maps_entity:
 		return new_longitude
 
 	def load_images_map(self, x, y):
-		down = self.calc_next_location_latitude(x,y,20,640,False)
-		up = self.calc_next_location_latitude(x,y,20,640,True)
-		right = self.calc_next_location_longitude(x,y,20,640,True)
-		left = self.calc_next_location_longitude(x,y,20,640,False)
+		down = self.calc_next_location_latitude(x,y,20,600,False)
+		up = self.calc_next_location_latitude(x,y,20,600,True)
+		right = self.calc_next_location_longitude(x,y,20,600,True)
+		left = self.calc_next_location_longitude(x,y,20,600,False)
 
 		up_left = self.get_and_store_location(up, left,"up_left.png")
 		up_center = self.get_and_store_location(up, y,"up_center.png")
