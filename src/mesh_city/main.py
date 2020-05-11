a8a65ba58fe3dfe16a23e9539337e72a8715c515
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-11 20:13:49 +0200

    Main module now have proper main method

--- src/mesh_city/main.py
+++ src/mesh_city/main.py
@@ -3,8 +3,14 @@ from mesh_city.imagery_provider.request_manager import RequestManager
 from mesh_city.imagery_provider.top_down_provider.ahn_provider import AhnProvider
 from mesh_city.imagery_provider.user_manager import UserManager
 
-user_manager = UserManager()
-provider = AhnProvider(user_manager)
-request_manager = RequestManager(user_manager)
-request_manager.load_images_map(51.998967, 4.373414)
-Application(request_manager)
+
+def main() -> None:
+	user_manager = UserManager()
+	AhnProvider(user_manager)
+	request_manager = RequestManager(user_manager)
+	request_manager.load_images_map(51.998967, 4.373414)
+	Application(request_manager)
+
+
+if __name__ == '__main__':
+	main()
