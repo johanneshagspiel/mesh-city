c63cc5bb966b0bd3228a30498f8382673d02f6cb
wfvmaas <wfvmaas@gmail.com>
2020-05-12 09:59:00 +0200

    Fixed some CI errors and added documentation

--- src/mesh_city/imagery_provider/request_manager.py
+++ src/mesh_city/imagery_provider/request_manager.py
@@ -7,6 +7,19 @@ import geopy
 from PIL import Image
 
 
+def calc_meters_per_px(latitude, zoom):
+	"""
+	Method which calculates the number of meters one pixel at this specific latitude and zoom level
+	represents.
+	:param latitude: respective latitude.
+	:param zoom: respective zoom level, accepts a value between 1 and 21. Urban areas have higher
+	zoom levels, whilst Anartica has a zoom level of 16.
+	:return: the number of meters one pixel represents in an image.
+	"""
+	meters_per_px = 156543.03392 * math.cos(latitude * math.pi / 180) / math.pow(2, zoom)
+	return meters_per_px
+
+
 class RequestManager:
 	temp_path = Path(__file__).parents[1]
 	images_folder_path = Path.joinpath(temp_path, "resources", "images")
@@ -55,6 +68,108 @@ class RequestManager:
 				self.concat_images(new_folder_path, request_number, tile_number - 1)
 				self.path_to_map_image = new_folder_path
 
+	def get_area(self, bottom_lat, left_long, top_lat, right_long, zoom, image_size):
+		"""
+		Method which calculates and retrieves the number of images that are necessary have a
+		complete imagery set of a certain geographical area. This area is defined by a bounding box.
+		The function checks whether the first coordinate inputted is "smaller" than the second
+		inputted coordinate.
+		:param bottom_lat: the bottom latitude / bottom left coordinate of the bounding box.
+		:param left_long: the left longitude / bottom left coordinate of the bounding box.
+		:param top_lat: the top latitude / top right coordinate of the bounding box.
+		:param right_long: the right longitude / top right coordinate of the bounding box.
+		:param zoom: the level of zoom (meters per pixel) the returned images will be.
+		:param image_size: the resolution of the images.
+		:return: false if the input is an illegal boundary box,
+		"""
+		if bottom_lat > top_lat or left_long > right_long:
+			return False
+
+		horizontal_width = geopy.distance.distance(
+			(bottom_lat, left_long), (bottom_lat, right_long)
+		).m
+		vertical_length = geopy.distance.distance((bottom_lat, left_long), (top_lat, left_long)).m
+
+		# print(
+		# 	"horizontal_width in meters = ",
+		# 	horizontal_width,
+		# 	"\nvertical_length in meters = ",
+		# 	vertical_length,
+		# 	"\nmeters per pixel = ",
+		# 	calc_meters_per_px(top_lat, zoom)
+		# )
+
+		# TODO do we need a different calculation for vertical? Bottom latitude is biggest: safe call
+		total_horizontal_pixels = horizontal_width / calc_meters_per_px(top_lat, zoom)
+		total_vertical_pixels = vertical_length / calc_meters_per_px(top_lat, zoom)
+
+		# print(
+		# 	"total_horizontal_pixels = ",
+		# 	total_horizontal_pixels,
+		# 	"\ntotal_vertical_pixels = ",
+		# 	total_vertical_pixels
+		# )
+
+		num_of_images_horizontal = int(math.ceil(total_horizontal_pixels / image_size))
+		num_of_images_vertical = int(math.ceil(total_vertical_pixels / image_size))
+
+		# print(
+		# 	"num_of_images_horizontal = ",
+		# 	num_of_images_horizontal,
+		# 	"\nnum_of_images_vertical = ",
+		# 	num_of_images_vertical
+		# )
+
+		latitude_first_image = self.calc_next_location_latitude(
+			bottom_lat, left_long, zoom, image_size / 2, False
+		)
+		longitude_first_image = self.calc_next_location_longitude(
+			bottom_lat, left_long, zoom, image_size / 2, False
+		)
+
+		current_latitude = latitude_first_image
+		current_longitude = longitude_first_image
+
+		# number_of_calls = 0
+
+		for vertical in range(num_of_images_vertical):
+			for horizontal in range(num_of_images_horizontal):
+				self.map_entity.get_and_store_location(current_latitude, current_longitude, False)
+
+				# print(current_latitude, ",", current_longitude)
+				# number_of_calls += 1
+				# print(number_of_calls)
+
+				current_longitude = self.calc_next_location_longitude(
+					current_latitude, current_longitude, zoom, image_size, False
+				)
+
+			current_longitude = longitude_first_image
+			current_latitude = self.calc_next_location_latitude(
+				current_latitude, current_longitude, zoom, image_size, False
+			)
+		return True
+
+	def calc_next_location_latitude(self, latitude, longitude, zoom, image_size_x, direction):
+		meters_per_px = calc_meters_per_px(latitude, zoom)
+		next_center_distance_meters = meters_per_px * image_size_x
+		if direction:
+			new_latitude = latitude + (next_center_distance_meters / 6378137) * (180 / math.pi)
+		else:
+			new_latitude = latitude - (next_center_distance_meters / 6378137) * (180 / math.pi)
+		return new_latitude
+
+	def calc_next_location_longitude(self, latitude, longitude, zoom, image_size_y, direction):
+		meters_per_px = calc_meters_per_px(latitude, zoom)
+		next_center_distance_meters = meters_per_px * image_size_y
+		if direction:
+			new_longitude = longitude + (next_center_distance_meters / 6378137) * (180 /
+				math.pi) / math.cos(latitude * math.pi / 180)
+		else:
+			new_longitude = longitude - (next_center_distance_meters / 6378137) * (180 /
+				math.pi) / math.cos(latitude * math.pi / 180)
+		return new_longitude
+
 	def calculate_locations(self, coordinates):
 		longitude = coordinates[0]
 		latitude = coordinates[1]
@@ -75,93 +190,6 @@ class RequestManager:
 			(down, latitude),
 			(down, right),
 		]  # yapf: disable
-
-	# box defined by bottom left and top right coordinate
-	def get_area(self, bottom_lat, left_long, top_lat, right_long, zoom, image_size):
-		horizontal_width = geopy.distance.distance(
-			(bottom_lat, left_long), (bottom_lat, right_long)
-		).m
-		vertical_length = geopy.distance.distance((bottom_lat, left_long), (top_lat, left_long)).m
-
-		print(
-			"horizontal_width in meters = ",
-			horizontal_width,
-			"\nvertical_length in meters = ",
-			vertical_length
-		)
-		print("meters per pixel = ", self.calc_meters_per_px(top_lat, zoom))
-
-		# TODO do we need a different calculation for vertical? Bottom latitude is biggest: safe call
-		total_horizontal_pixels = horizontal_width / self.calc_meters_per_px(top_lat, zoom)
-		total_vertical_pixels = vertical_length / self.calc_meters_per_px(top_lat, zoom)
-
-		print(
-			"total_horizontal_pixels = ",
-			total_horizontal_pixels,
-			"\ntotal_vertical_pixels = ",
-			total_vertical_pixels
-		)
-
-		num_of_images_horizontal = int(math.ceil(total_horizontal_pixels / image_size))
-		num_of_images_vertical = int(math.ceil(total_vertical_pixels / image_size))
-
-		print(
-			"num_of_images_horizontal = ",
-			num_of_images_horizontal,
-			"\nnum_of_images_vertical = ",
-			num_of_images_vertical
-		)
-
-		latitude_first_image = self.calc_next_location_latitude(
-			bottom_lat, left_long, zoom, image_size / 2, False
-		)
-		# bottom_latitude + ((top_latitude - bottom_latitude) / (num_of_images_vertical * 2))
-		longitude_first_image = self.calc_next_location_longitude(
-			bottom_lat, left_long, zoom, image_size / 2, False
-		)
-		# left_longitude + ((left_longitude - right_longitude) / (num_of_images_horizontal * 2))
-
-		current_latitude = latitude_first_image
-		current_longitude = longitude_first_image
-
-		number_of_calls = 0
-
-		for vertical in range(num_of_images_vertical):
-			for horizontal in range(num_of_images_horizontal):
-				self.map_entity.get_and_store_location(current_latitude, current_longitude, False)
-				print(current_latitude, ",", current_longitude)
-
-				number_of_calls += 1
-				print(number_of_calls)
-
-				current_longitude = self.calc_next_location_longitude(
-					current_latitude, current_longitude, zoom, image_size, False
-				)
-
-			current_longitude = longitude_first_image
-			current_latitude = self.calc_next_location_latitude(
-				current_latitude, current_longitude, zoom, image_size, False
-			)
-
-	def calc_next_location_latitude(self, latitude, longitude, zoom, image_size_x, direction):
-		meters_per_px = 156543.03392 * math.cos(latitude * math.pi / 180) / math.pow(2, zoom)
-		next_center_distance_meters = meters_per_px * image_size_x
-		if direction:
-			new_latitude = latitude + (next_center_distance_meters / 6378137) * (180 / math.pi)
-		else:
-			new_latitude = latitude - (next_center_distance_meters / 6378137) * (180 / math.pi)
-		return new_latitude
-
-	def calc_next_location_longitude(self, latitude, longitude, zoom, image_size_y, direction):
-		meters_per_px = 156543.03392 * math.cos(latitude * math.pi / 180) / math.pow(2, zoom)
-		next_center_distance_meters = meters_per_px * image_size_y
-		if direction:
-			new_longitude = longitude + (next_center_distance_meters / 6378137) * (180 /
-				math.pi) / math.cos(latitude * math.pi / 180)
-		else:
-			new_longitude = longitude - (next_center_distance_meters / 6378137) * (180 /
-				math.pi) / math.cos(latitude * math.pi / 180)
-		return new_longitude
 
 	def concat_images(self, new_folder_path, request, tile_number):
 		up_left = Image.open(
