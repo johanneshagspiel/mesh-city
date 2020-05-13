e213313816967dd274b1d227c626685053a471e1
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-05-13 11:46:58 +0200

    Changed the gui a bit

--- src/mesh_city/gui/application.py
+++ src/mesh_city/gui/application.py
@@ -29,30 +29,77 @@ class Application:
 		self.master.geometry("")
 
 		# Definition of UI of main window
-		self.canvas = Canvas(self.master, width=651, height=636)
+		self.canvas = Canvas(self.master, width=651, height=650)
 		self.canvas.grid(column=3, columnspan=30, row=0, rowspan=30)
 
-		self.file_entry = Entry(self.master)
-		self.file_entry.grid(row=0, columnspan=3)
-		set_entry(self.file_entry, self.file)
-		btn1 = Button(self.master, text="Change output folder", command=self.select_dir)
-		btn1.grid(row=1, columnspan=3)
+		side_bar = Label(self.canvas, width=15, height=645, text="")
+		canvas_side_bar = self.canvas.create_window(0, 0, window=side_bar)
 
-		Label(self.master, text="Quota").grid(row=2)
-		Label(self.master, text="Latitude").grid(row=3)
-		Label(self.master, text="Longitude").grid(row=4)
+		search_button = Button(self.canvas, text="Search", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_search_button = self.canvas.create_window(28, 31, window=search_button)
 
-		self.lat_entry = Entry(self.master)
-		self.long_entry = Entry(self.master)
-		self.quota_entry = Entry(self.master)
-		self.quota_entry.grid(row=2, column=1)
-		self.lat_entry.grid(row=3, column=1)
-		self.long_entry.grid(row=4, column=1)
+		test1_button = Button(self.canvas, text="Test1", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test1_button = self.canvas.create_window(28, 90, window=test1_button)
 
-		btn2 = Button(
-			self.master, text="Extract imagery to output folder", command=self.request_data
-		)
-		btn2.grid(row=5, columnspan=3)
+		test2_button = Button(self.canvas, text="Test2", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test2_button = self.canvas.create_window(28, 149, window=test2_button)
+
+		test3_button = Button(self.canvas, text="Test3", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test3_button = self.canvas.create_window(28, 208, window=test3_button)
+
+		test4_button = Button(self.canvas, text="Test4", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test4_button = self.canvas.create_window(28, 267, window=test4_button)
+
+		test5_button = Button(self.canvas, text="Test5", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test5_button = self.canvas.create_window(28, 326, window=test5_button)
+
+		test6_button = Button(self.canvas, text="Test6", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test6_button = self.canvas.create_window(28, 385, window=test6_button)
+
+		test7_button = Button(self.canvas, text="Test7", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test7_button = self.canvas.create_window(28, 444, window=test7_button)
+
+		test8_button = Button(self.canvas, text="Test8", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test8_button = self.canvas.create_window(28, 503, window=test8_button)
+
+		test9_button = Button(self.canvas, text="Test9", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test9_button = self.canvas.create_window(28, 562, window=test9_button)
+
+		test10_button = Button(self.canvas, text="Test10", width=6, height=3,
+		                 command=None, bg="grey")
+		canvas_test10_button = self.canvas.create_window(28, 621, window=test10_button)
+
+		# self.file_entry = Entry(self.master)
+		# self.file_entry.grid(row=0, columnspan=3)
+		# set_entry(self.file_entry, self.file)
+		# btn1 = Button(self.master, text="Change output folder", command=self.select_dir)
+		# btn1.grid(row=1, columnspan=3)
+		#
+		# Label(self.master, text="Quota").grid(row=2)
+		# Label(self.master, text="Latitude").grid(row=3)
+		# Label(self.master, text="Longitude").grid(row=4)
+		#
+		# self.lat_entry = Entry(self.master)
+		# self.long_entry = Entry(self.master)
+		# self.quota_entry = Entry(self.master)
+		# self.quota_entry.grid(row=2, column=1)
+		# self.lat_entry.grid(row=3, column=1)
+		# self.long_entry.grid(row=4, column=1)
+		#
+		# btn2 = Button(
+		# 	self.master, text="Extract imagery to output folder", command=self.request_data
+		# )
+		# btn2.grid(row=5, columnspan=3)
 
 		self.user_info_handler = UserInfoHandler()
 		if self.user_info_handler.file_exists():
@@ -65,8 +112,8 @@ class Application:
 			user_info=self.user_info, quota_manager=self.quota_manager
 		)
 
-		# large_photo = self.load_large_image()
-		# self.load_large_image_on_map(self.canvas, large_photo)
+		large_photo = self.load_large_image()
+		self.load_large_image_on_map(self.canvas, large_photo)
 
 		mainloop()
 
