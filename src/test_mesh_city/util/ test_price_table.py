6b8c016fe49e66c953937e53fcba213fc7c39919
tbvanderwoude <t.b.vanderwoude@student.tudelft.nl>
2020-05-21 21:48:25 +0200

    Reformatted code

--- src/test_mesh_city/util/ test_price_table.py
+++ src/test_mesh_city/util/ test_price_table.py
@@ -16,28 +16,26 @@ class PriceTableTest(unittest.TestCase):
 
 	@parameterized.expand(
 		[
-		["google_maps", "static_map", 0, 1, 100000,0.002],
-		["google_maps", "static_map", 100000, 1, 1000000,0.002],
-		["google_maps", "static_map", 100001, 1, 1000000,0.0016],
-		["google_maps", "static_map", 500000, 1, 1000000,0.0016],
-		["google_maps", "geocoding", 0, 1, 100000,0.005],
-		["google_maps", "geocoding", 100000, 1, 1000000, 0.005],
-		["google_maps", "geocoding", 100001, 1, 1000000, 0.004],
-		["google_maps", "geocoding", 500000, 1, 1000000, 0.004],
-		["mapbox", "static_map", 0, 1, 1000000, 0],
-		["mapbox", "static_map", 50000, 1, 10000000,0],
-		["mapbox", "static_map", 50001, 1, 10000000, 0.001],
-		["mapbox", "static_map", 500000, 1, 10000000, 0.001],
-		["mapbox", "static_map", 500001, 1, 10000000, 0.0008],
-		["mapbox", "static_map", 1000000, 1, 10000000, 0.0008],
-		["mapbox", "static_map", 1000001, 1, 10000000, 0.0006],
-		["mapbox", "geocoding", 0, 1, 1000000, 0],
-		["mapbox", "geocoding", 100000, 1, 10000000,0],
-		["mapbox", "geocoding", 100001, 1, 10000000, 0.00075],
-		["mapbox", "geocoding", 500000, 1, 10000000, 0.00075],
-		["mapbox", "geocoding", 500001, 1, 10000000, 0.0006],
-		["mapbox", "geocoding", 1000000, 1, 10000000, 0.0006],
-		["mapbox", "geocoding", 1000001, 1, 10000000, 0.00045],
+		["google_maps", "static_map", 0, 1, 100000,
+		0.002], ["google_maps", "static_map", 100000, 1, 1000000,
+		0.002], ["google_maps", "static_map", 100001, 1, 1000000,
+		0.0016], ["google_maps", "static_map", 500000, 1, 1000000,
+		0.0016], ["google_maps", "geocoding", 0, 1, 100000,
+		0.005], ["google_maps", "geocoding", 100000, 1, 1000000, 0.005],
+		["google_maps", "geocoding", 100001, 1, 1000000, 0.004], [
+		"google_maps", "geocoding", 500000, 1, 1000000, 0.004
+		], ["mapbox", "static_map", 0, 1, 1000000, 0], ["mapbox", "static_map", 50000, 1, 10000000,
+		0], ["mapbox", "static_map", 50001, 1, 10000000, 0.001], [
+		"mapbox", "static_map", 500000, 1, 10000000, 0.001
+		], ["mapbox", "static_map", 500001, 1, 10000000, 0.0008], [
+		"mapbox", "static_map", 1000000, 1, 10000000, 0.0008
+		], ["mapbox", "static_map", 1000001, 1, 10000000, 0.0006],
+		["mapbox", "geocoding", 0, 1, 1000000, 0], ["mapbox", "geocoding", 100000, 1, 10000000,
+		0], ["mapbox", "geocoding", 100001, 1, 10000000, 0.00075], [
+		"mapbox", "geocoding", 500000, 1, 10000000, 0.00075
+		], ["mapbox", "geocoding", 500001, 1, 10000000, 0.0006], [
+		"mapbox", "geocoding", 1000000, 1, 10000000, 0.0006
+		], ["mapbox", "geocoding", 1000001, 1, 10000000, 0.00045],
 		]
 	)
 	def test_calculate_action_price(self, api_type, action_type, usage, requests, quota, price):
@@ -47,10 +45,34 @@ class PriceTableTest(unittest.TestCase):
 		)
 
 	def test_undefined_usage_range(self):
-		self.assertRaises(ValueError, PriceTableUtil.calculate_action_price,"google_maps", "geocoding", 500001, 1, 1000000)
+		self.assertRaises(
+			ValueError,
+			PriceTableUtil.calculate_action_price,
+			"google_maps",
+			"geocoding",
+			500001,
+			1,
+			1000000
+		)
 
 	def test_undefined_api(self):
-		self.assertRaises(ValueError, PriceTableUtil.calculate_action_price,"map_map", "geocoding", 500001, 1, 1000000)
+		self.assertRaises(
+			ValueError,
+			PriceTableUtil.calculate_action_price,
+			"map_map",
+			"geocoding",
+			500001,
+			1,
+			1000000
+		)
 
 	def test_undefined_service(self):
-		self.assertRaises(ValueError, PriceTableUtil.calculate_action_price,"google_maps", "undefined_test_service", 500001, 1, 1000000)
+		self.assertRaises(
+			ValueError,
+			PriceTableUtil.calculate_action_price,
+			"google_maps",
+			"undefined_test_service",
+			500001,
+			1,
+			1000000
+		)
