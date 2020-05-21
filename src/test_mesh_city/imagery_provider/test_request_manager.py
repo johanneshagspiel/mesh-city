5619fdf634bbaa31a1f10d33a9669ece13ca11e8
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-21 15:50:00 +0200

    Cleaned up & restructured req manager tests

--- src/test_mesh_city/imagery_provider/test_request_manager.py
+++ src/test_mesh_city/imagery_provider/test_request_manager.py
@@ -7,82 +7,23 @@ from unittest.mock import ANY, call, Mock
 
 from mesh_city.imagery_provider.request_manager import RequestManager
 from mesh_city.imagery_provider.top_down_provider.google_maps_provider import GoogleMapsProvider
-from mesh_city.logs.log_manager import LogManager
-from mesh_city.user.quota_manager import QuotaManager
-from mesh_city.user.user_info import UserInfo
 from mesh_city.util.geo_location_util import GeoLocationUtil
-from mesh_city.util.image_util import ImageUtil
 
 
 class TestRequestManager(unittest.TestCase):
 
-	class MockTopDownProvider:
-
-		def __init__(self):
-			self.max_zoom = None
-			self.max_side_resolution_image = None
-			self.padding = None
-			self.name = None
-			self.given_arguments = {"get_and_store_location": []}
-
-		def get_and_store_location(
-			self, latitude, longitude, zoom, filename, new_folder_path, width=None, height=None
-		):
-			self.given_arguments["get_and_store_location"].append(
-				{
-				"latitude": latitude,
-				"longitude": longitude,
-				"zoom": zoom,
-				"new_folder_path": new_folder_path,
-				"filename": filename,
-				"width": width,
-				"height": height,
-				}
-			)
-
-	class MockImageUtil:
-
-		def __init__(self):
-			self.given_arguments = {"concat_images": []}
-
-		def concat_images(self, new_folder_path, request, tile_number):
-			self.given_arguments["concat_images"].append(
-				{
-				"new_folder_path": new_folder_path, "request": request, "tile_number": tile_number,
-				}
-			)
-
-	class MockLogManager:
-
-		def __init__(self):
-			self.given_arguments = {"write_entry_log": []}
-
-		def write_entry_log(self, log_entry):
-			self.given_arguments["write_entry_log"].append({"log_entry": log_entry})
-
-		def get_request_number(self):
-			return 1
+	resource_path = Path(__file__).parents[1].joinpath("resources")
 
 	def setUp(self):
-		self.resource_path = Path(__file__).parents[1].joinpath("resources")
-		self.user_info = UserInfo(
-			"Blue", "AIzaSyD9cfAeQKFniipqRUgkcYy1sAtGXJYxNF4", 500, 25, 1452, 8, 15, 10, 55, 42
-		)
-		self.quota_manager = QuotaManager(self.user_info)
-		self.request_manager = RequestManager(
-			user_info=self.user_info,
-			quota_manager=self.quota_manager,
-			map_entity=GoogleMapsProvider(user_info=self.user_info, quota_manager=self.quota_manager),
-			log_manager=LogManager(),
-			image_util=ImageUtil(),
-			geo_location_util=GeoLocationUtil(),
-		)
-		self.two_coordinate_input = (-22.824637, -43.242729), (-22.821384, -43.238813), 20
+		self.top_down_provider = Mock()
+		self.top_down_provider.max_side_resolution_image = 640
+
+		self.two_coordinate_input = ((-22.824637, -43.242729), (-22.821384, -43.238813), 20)
 		self.correct_answer = (
 			(25, 5, 5),
 			[
-			((-22.824266172626547, -43.24232666864772), (0,0)),
-			((-22.824266172626547, -43.24152200594317), (1,0)),
+			((-22.824266172626547, -43.24232666864772), (0, 0)),
+			((-22.824266172626547, -43.24152200594317), (1, 0)),
 			((-22.824266172626547, -43.240717343238614), (2, 0)),
 			((-22.824266172626547, -43.23991268053406), (3, 0)),
 			((-22.824266172626547, -43.23910801782951), (4, 0)),
@@ -106,7 +47,7 @@ class TestRequestManager(unittest.TestCase):
 			((-22.821299521316735, -43.240717343238614), (2, 4)),
 			((-22.821299521316735, -43.23991268053406), (3, 4)),
 			((-22.821299521316735, -43.23910801782951), (4, 4)),
-			]
+			],
 		)  # yapf: disable
 
 	def tearDown(self):
@@ -116,40 +57,75 @@ class TestRequestManager(unittest.TestCase):
 			else:
 				item.unlink()
 
+	def _create_request_manager(
+		self, top_down_provider=Mock(), log_manager=Mock(), geo_location_util=Mock()
+	):
+		return RequestManager(
+			user_info=Mock(),
+			quota_manager=Mock(),
+			top_down_provider=top_down_provider,
+			log_manager=log_manager,
+			image_util=Mock(),
+			geo_location_util=geo_location_util,
+			resource_path=self.resource_path,
+		)
+
 	def test_calculate_centre_coordinates_two_coordinate_input_correct(self):
-		list_of_coordinates = self.request_manager.calculate_centre_coordinates_two_coordinate_input(
+		map_entity = Mock(spec=GoogleMapsProvider, wraps=Mock())
+		map_entity.max_side_resolution_image = 640
+		request_manager = self._create_request_manager(
+			top_down_provider=map_entity, geo_location_util=GeoLocationUtil(),
+		)
+
+		list_of_coordinates = request_manager.calculate_centre_coordinates_two_coordinate_input(
 			self.two_coordinate_input[0], self.two_coordinate_input[1], self.two_coordinate_input[2]
 		)
+
 		self.assertEqual(self.correct_answer, list_of_coordinates)
 
 	def test_calculate_centre_coordinates_two_coordinate_input_turned_around(self):
-		list_of_coordinates = self.request_manager.calculate_centre_coordinates_two_coordinate_input(
+		map_entity = Mock(spec=GoogleMapsProvider, wraps=Mock())
+		map_entity.max_side_resolution_image = 640
+		request_manager = self._create_request_manager(
+			top_down_provider=map_entity, geo_location_util=GeoLocationUtil(),
+		)
+
+		list_of_coordinates = request_manager.calculate_centre_coordinates_two_coordinate_input(
 			self.two_coordinate_input[1], self.two_coordinate_input[0], self.two_coordinate_input[2]
 		)
+
 		self.assertEqual(self.correct_answer, list_of_coordinates)
 
 	def test_calculate_number_of_requested_images_two_coordinate_input(self):
-		number_of_images = self.request_manager.calculate_number_of_requested_images_two_coordinate_input(
+		number_of_images = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil(),
+		).calculate_number_of_requested_images_two_coordinate_input(
 			self.two_coordinate_input[0], self.two_coordinate_input[1], self.two_coordinate_input[2]
 		)
 		self.assertEqual(25, number_of_images)
 
 	def test_calculate_number_of_requested_images_two_coordinate_input_turned_around(self):
-		number_of_images = self.request_manager.calculate_number_of_requested_images_two_coordinate_input(
+		number_of_images = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil(),
+		).calculate_number_of_requested_images_two_coordinate_input(
 			self.two_coordinate_input[1], self.two_coordinate_input[0], self.two_coordinate_input[2]
 		)
 		self.assertEqual(25, number_of_images)
 
 	def test_calculate_number_of_requested_images_two_coordinate_input_same_latitude(self):
-		number_of_images = self.request_manager.calculate_number_of_requested_images_two_coordinate_input(
+		number_of_images = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil(),
+		).calculate_number_of_requested_images_two_coordinate_input(
 			self.two_coordinate_input[0],
 			(self.two_coordinate_input[0][0], self.two_coordinate_input[1][1]),
-			self.two_coordinate_input[2]
+			self.two_coordinate_input[2],
 		)
 		self.assertEqual(5, number_of_images)
 
 	def test_calculate_number_of_requested_images_two_coordinate_input_same_longitude(self):
-		number_of_images = self.request_manager.calculate_number_of_requested_images_two_coordinate_input(
+		number_of_images = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil(),
+		).calculate_number_of_requested_images_two_coordinate_input(
 			self.two_coordinate_input[0],
 			(self.two_coordinate_input[1][0], self.two_coordinate_input[0][1]),
 			self.two_coordinate_input[2]
@@ -157,20 +133,11 @@ class TestRequestManager(unittest.TestCase):
 		self.assertEqual(5, number_of_images)
 
 	def test_single_request(self):
-		top_down_provider = Mock()
-		request_manager = RequestManager(
-			user_info=Mock(),
-			quota_manager=Mock(),
-			map_entity=top_down_provider,
-			log_manager=Mock(),
-			image_util=Mock(),
-			geo_location_util=Mock(),
-			resource_path=self.resource_path,
-		)
+		request_manager = self._create_request_manager(top_down_provider=self.top_down_provider)
 
 		request_manager.make_single_request((52.010442, 4.357480), 1.0, 400, 600)
 
-		top_down_provider.get_and_store_location.assert_called_once_with(
+		self.top_down_provider.get_and_store_location.assert_called_once_with(
 			latitude=52.010442,
 			longitude=4.357480,
 			zoom=1.0,
@@ -181,22 +148,15 @@ class TestRequestManager(unittest.TestCase):
 		)
 
 	def test_bounding_box_request_bottom_left_top_right(self):
-		top_down_provider = Mock()
-		top_down_provider.max_zoom = 14.0
-		top_down_provider.max_side_resolution_image = 500
-		request_manager = RequestManager(
-			user_info=Mock(),
-			quota_manager=Mock(),
-			map_entity=top_down_provider,
-			log_manager=Mock(),
-			image_util=Mock(),
-			geo_location_util=GeoLocationUtil(),
-			resource_path=self.resource_path,
+		self.top_down_provider.max_zoom = 14.0
+		self.top_down_provider.max_side_resolution_image = 500
+		request_manager = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil()
 		)
 
 		request_manager.make_request_two_coordinates((51.989954, 4.330746), (52.021186, 4.374115))
 
-		top_down_provider.get_and_store_location.assert_has_calls(
+		self.top_down_provider.get_and_store_location.assert_has_calls(
 			calls=[
 			call(
 			latitude=52.00316762660327,
@@ -231,22 +191,15 @@ class TestRequestManager(unittest.TestCase):
 		)
 
 	def test_request_block_centre_coordinates(self):
-		top_down_provider = Mock()
-		top_down_provider.padding = 0
-		top_down_provider.name = "mock"
-		request_manager = RequestManager(
-			user_info=Mock(),
-			quota_manager=Mock(),
-			map_entity=top_down_provider,
-			log_manager=Mock(),
-			image_util=Mock(),
-			geo_location_util=GeoLocationUtil(),
-			resource_path=self.resource_path,
+		self.top_down_provider.padding = 0
+		self.top_down_provider.name = "mock"
+		request_manager = self._create_request_manager(
+			top_down_provider=self.top_down_provider, geo_location_util=GeoLocationUtil()
 		)
 
 		request_manager.make_request_for_block(centre_coordinates=(51.989954, 4.330746), zoom=20.0)
 
-		top_down_provider.get_and_store_location.assert_has_calls(
+		self.top_down_provider.get_and_store_location.assert_has_calls(
 			calls=[
 			call(
 			latitude=51.98942545493587,
@@ -281,28 +234,22 @@ class TestRequestManager(unittest.TestCase):
 		)
 
 	def test_request_block_bounding_box(self):
-		top_down_provider = Mock()
-		top_down_provider.padding = 0
-		top_down_provider.name = "mock"
-		top_down_provider.max_side_resolution_image = 640
-		top_down_provider.max_zoom = 14.0
+		self.top_down_provider.padding = 0
+		self.top_down_provider.name = "mock"
+		self.top_down_provider.max_zoom = 14.0
 		log_manager = Mock()
 		log_manager.get_request_number.return_value = 1
-		request_manager = RequestManager(
-			user_info=Mock(),
-			quota_manager=Mock(),
-			map_entity=top_down_provider,
+		request_manager = self._create_request_manager(
+			top_down_provider=self.top_down_provider,
 			log_manager=log_manager,
-			image_util=Mock(),
 			geo_location_util=GeoLocationUtil(),
-			resource_path=self.resource_path,
 		)
 
 		request_manager.make_request_for_block(
 			centre_coordinates=(51.989954, 4.330746, 52.021186, 4.374115), zoom=14.0
 		)
 
-		top_down_provider.get_and_store_location.assert_has_calls(
+		self.top_down_provider.get_and_store_location.assert_has_calls(
 			calls=[
 			call(
 			latitude=52.00686744205219,
