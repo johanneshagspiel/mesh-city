32e4f407a213e528323ee125426ad6745345869d
Reinout Meliesie <r.meliesie@student.tudelft.nl>
2020-05-26 14:09:56 +0200

    Re-added req man tests that still work

--- src/mesh_city/gui/load_window/load_window.py
+++ src/mesh_city/gui/load_window/load_window.py
@@ -52,14 +52,20 @@ class LoadWindow:
 		:return: nothing
 		"""
 
-		self.application.file_handler.folder_overview["active_tile_path"][0] = \
-                                                                           Path.joinpath(self.application.file_handler.folder_overview["image_path"][0], name_directory, "0_tile_0_0")
+		self.application.file_handler.folder_overview["active_tile_path"][0] = Path.joinpath(
+			self.application.file_handler.folder_overview["image_path"][0],
+			name_directory,
+			"0_tile_0_0"
+		)
 
-		self.application.file_handler.folder_overview["active_image_path"][0] = \
-                                                                           Path.joinpath(self.application.file_handler.folder_overview["image_path"][0], name_directory, "0_tile_0_0")
+		self.application.file_handler.folder_overview["active_image_path"][0] = Path.joinpath(
+			self.application.file_handler.folder_overview["image_path"][0],
+			name_directory,
+			"0_tile_0_0"
+		)
 
-		self.application.file_handler.folder_overview["active_request_path"][0] = \
-                                                                           self.application.file_handler.folder_overview["active_tile_path"][0].parents[0]
+		self.application.file_handler.folder_overview["active_request_path"][
+			0] = self.application.file_handler.folder_overview["active_tile_path"][0].parents[0]
 
 		self.mainscreen.update_image()
 		self.mainscreen.layer_active = "normal"
