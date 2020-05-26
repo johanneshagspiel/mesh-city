b8b1526db31386037f178a2f45674febaf5bad93
Borna <B.Salarian@student.tudelft.nl>
2020-05-26 11:56:43 +0200

    Fixed the mapbox tests so that now they work with the refactored user.

--- src/test_mesh_city/imagery_provider/top_down_provider/test_mapbox_provider.py
+++ src/test_mesh_city/imagery_provider/top_down_provider/test_mapbox_provider.py
@@ -1,78 +1,57 @@
-import requests
 import unittest
-from unittest import mock
+from datetime import datetime
 from pathlib import Path
+from unittest import mock
+
+import requests
 
 from mesh_city.imagery_provider.top_down_provider.mapbox_provider import MapboxProvider
-from mesh_city.user.quota_manager import QuotaManager
-from mesh_city.user.user_info import UserInfo
+from mesh_city.user.entities.image_provider_entity import ImageProviderEntity
+from mesh_city.user.entities.user_entity import UserEntity
+from mesh_city.util.file_handler import FileHandler
+
 
 class mapboxproviderTest(unittest.TestCase):
+
 	def setUp(self):
-		self.user_info_init = UserInfo("Blue", "AIzaSyD9cfAeQKFniipqRUgkcYy1sAtGXJYxNF4"
-				                               , 500, 25, 1452, 8, 15, 10, 55, 42)
-		self.quota_manager_init = QuotaManager(self.user_info_init)
-		self.mapbox = MapboxProvider(self.user_info_init,self.quota_manager_init)
+		self.provider = ImageProviderEntity(
+			FileHandler(),
+			type_map_provider="google_maps",
+			api_key="AIzaSyD9cfAeQKFniipqRUgkcYy1sAtGXJYxNF4",
+			quota=500,
+			usage=None,
+			date_reset=datetime(2019, 2, 28)
+		)
+
+		self.mapbox = MapboxProvider(self.provider)
 		self.longitude = (11.999212921106265, 11.999195337295573)
 		self.latitude = (12.000787078893735, 12.000804662704427)
 		self.zoom = 2
-		self.file_path = Path.joinpath(
-			Path(__file__).parents[2], "resources"
-		)
+		self.file_path = Path.joinpath(Path(__file__).parents[2], "resources")
 
 	def mock_response(self):
 		mock_resp = mock.Mock()
-		path = Path.joinpath(
-			Path(__file__).parents[2], "resources", "test_response_image.png"
-		)
-		with open(path,"rb") as img:
+		path = Path.joinpath(Path(__file__).parents[2], "resources", "test_response_image.png")
+		with open(path, "rb") as img:
 			mock_resp.content = img.read()
 		return mock_resp
 
 	def test_get_and_store(self):
 		mapbox = self.mapbox
-		mock_path = Path.joinpath(
-			Path(__file__).parents[2], "resources", "test_response_image.png"
+		mapbox.get_and_store_location(
+			latitude=self.latitude,
+			longitude=self.longitude,
+			zoom=self.zoom,
+			filename="test_google_top_down.png",
+			new_folder_path=self.file_path,
+			response=self.mock_response()
 		)
-		mapbox.get_and_store_location(latitude=self.latitude, longitude=self.longitude
-		                             , zoom=self.zoom, filename="test_google_top_down.png"
-		                             , new_folder_path=self.file_path
-		                             , response=self.mock_response())
-		with open(self.file_path) as image_one:
-			recieved_image = image_one.read()
-		with open(mock_path) as image_two:
+		mock_path = Path.joinpath(Path(__file__).parents[2], "resources", "test_response_image.png")
+		image_path = self.file_path = Path.joinpath(
+			Path(__file__).parents[2], "resources", "test_google_top_down.png"
+		)
+		with open(image_path, "rb") as image_one, open(mock_path, "rb") as image_two:
+			received_image = image_one.read()
 			mock_image = image_two.read()
-		self.assertEqual(recieved_image,mock_image)
-# class GoogleMapsProviderTest(unittest.TestCase):
-#
-# 	def setUp(self):
-# 		self.user_info_init = UserInfo("Blue", "AIzaSyD9cfAeQKFniipqRUgkcYy1sAtGXJYxNF4"
-# 		                               , 500, 25, 1452, 8, 15, 10, 55, 42)
-# 		self.quota_manager_init = QuotaManager(self.user_info_init)
-# 		# self.top_down_prov = TopDownProvider(self.user_info_init,self.quota_manager_init)
-# 		self.google_maps_prov = GoogleMapsProvider(self.user_info_init, self.quota_manager_init)
-# 		self.longitude = (11.999212921106265, 11.999195337295573)
-# 		self.latitude = (12.000787078893735, 12.000804662704427)
-# 		self.zoom = 2
-# 		self.file_path = Path.joinpath(
-# 			Path(__file__).parents[2], "resources"
-# 		)
-#
-# 	def mock_response(self):
-# 		mock_resp = mock.Mock()
-# 		path = Path.joinpath(
-# 			Path(__file__).parents[2], "resources", "test_response_image.png"
-# 		)
-# 		with open(path,"rb") as img:
-# 			mock_resp.content = img.read()
-#
-# 		return mock_resp
-#
-# 	def test_get_and_store(self):
-# 		g_map = self.google_maps_prov
-# 		usage_before = self.user_info_init.usage
-# 		g_map.get_and_store_location(latitude=self.latitude, longitude=self.longitude
-# 		                             , zoom=self.zoom, filename="test_google_top_down.png"
-# 		                             , new_folder_path=self.file_path
-# 		                             , response=self.mock_response())
-# 		self.assertEqual( (usage_before+1), self.user_info_init.usage)
+
+		self.assertEqual(received_image, mock_image)
