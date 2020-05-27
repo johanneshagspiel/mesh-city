3626d879cbbfcd97880e67ded2d2af7eb89c9ec9
tbvanderwoude <t.b.vanderwoude@student.tudelft.nl>
2020-05-27 10:05:31 +0200

    Changed a few more calls for consistency

--- src/test_mesh_city/util/test_image_util.py
+++ src/test_mesh_city/util/test_image_util.py
@@ -17,15 +17,15 @@ class TestImageUtil(unittest.TestCase):
 	def test_concat_grid(self):
 		img_util = ImageUtil()
 		test_images = [
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 255, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 0, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 0, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 0, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 0, 0]]])
+			self.matrix_to_image([[[255, 255, 255], [255, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 255, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 0, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 0, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 0, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 0, 0]]])
 		]
 		for index in range(1, 10):
 			test_images[index -
@@ -105,7 +105,7 @@ class TestImageUtil(unittest.TestCase):
 
 	def test_1x1_grid(self):
 		img_util = ImageUtil()
-		test_image = TestImageUtil.matrix_to_image([[[255, 255, 255]]])
+		test_image = self.matrix_to_image([[[255, 255, 255]]])
 		result_array = np.asarray(img_util.concat_image_grid(1, 1, [test_image]))
 		target_array = np.asarray(test_image)
 		self.assertEqual(result_array.shape, target_array.shape)
@@ -113,14 +113,12 @@ class TestImageUtil(unittest.TestCase):
 
 	def test_2x2_grid(self):
 		img_util = ImageUtil()
-		test_image_1 = TestImageUtil.matrix_to_image([[[255, 255, 255]]])
-		test_image_2 = TestImageUtil.matrix_to_image([[[255, 255, 0]]])
-		test_image_3 = TestImageUtil.matrix_to_image([[[255, 0, 255]]])
-		test_image_4 = TestImageUtil.matrix_to_image([[[255, 0, 0]]])
+		test_image_1 = self.matrix_to_image([[[255, 255, 255]]])
+		test_image_2 = self.matrix_to_image([[[255, 255, 0]]])
+		test_image_3 = self.matrix_to_image([[[255, 0, 255]]])
+		test_image_4 = self.matrix_to_image([[[255, 0, 0]]])
 		result_array = np.asarray(
-			img_util.concat_image_grid(
-			2, 2, [test_image_1, test_image_2, test_image_3, test_image_4]
-			)
+			img_util.concat_image_grid(2, 2, [test_image_1, test_image_2, test_image_3, test_image_4])
 		)
 		target_array = np.array([[[255, 255, 255], [255, 255, 0]], [[255, 0, 255], [255, 0, 0]]])
 		self.assertEqual(result_array.shape, target_array.shape)
@@ -129,15 +127,15 @@ class TestImageUtil(unittest.TestCase):
 	def test_3x3_grid(self):
 		img_util = ImageUtil()
 		test_images = [
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 255, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 0, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [255, 0, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 255, 0]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 0, 255]]]),
-			TestImageUtil.matrix_to_image([[[255, 255, 255], [0, 0, 0]]])
+			self.matrix_to_image([[[255, 255, 255], [255, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 255, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 0, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [255, 0, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 255, 0]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 0, 255]]]),
+			self.matrix_to_image([[[255, 255, 255], [0, 0, 0]]])
 		]
 		result_array = np.asarray(img_util.concat_image_grid(3, 3, test_images))
 		target_array = np.array(
