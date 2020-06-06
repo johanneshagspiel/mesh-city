cdf68f79e8e89587180a14d26e98de8d79ea7b02
tbvanderwoude <t.b.vanderwoude@student.tudelft.nl>
2020-06-06 17:14:56 +0200

    Refactored image util

--- src/mesh_city/util/image_util.py
+++ src/mesh_city/util/image_util.py
@@ -37,83 +37,6 @@ class ImageUtil:
 		down_center = Image.open(image_list[7])
 		down_right = Image.open(image_list[8])
 
-		level_0 = self.get_concat_horizontally(
-			image_1=self.get_concat_horizontally(image_1=up_left, image_2=up_center), image_2=up_right
-		)
-		level_1 = self.get_concat_horizontally(
-			image_1=self.get_concat_horizontally(image_1=center_left, image_2=center_center),
-			image_2=center_right
-		)
-		level_2 = self.get_concat_horizontally(
-			image_1=self.get_concat_horizontally(image_1=down_left, image_2=down_center),
-			image_2=down_right
-		)
-
-		temp_concat_image = self.get_concat_vertically(
-			image_1=self.get_concat_vertically(image_1=level_2, image_2=level_1), image_2=level_0
-		)
-
-		return temp_concat_image
-
-	# pylint: disable=C0200
-	def combine_images_list(self, image_list, iteration_amount):
-		"""
-		The method to concatenate all the images from a list
-		:param image_list: the list with all the images to be concatenated
-		:param iteration_amount: many images are in one row of the concatenated image
-		:return: the concatenated image
-		"""
-
-		temp_list = []
-		temp_entry = None
-		counter = 0
-
-		for number in range(0, len(image_list)):
-
-			if counter == 0:
-				temp_entry = image_list[number]
-				counter += 1
-			else:
-				new_temp_1 = image_list[number]
-				new_temp = self.get_concat_horizontally(temp_entry, new_temp_1)
-				temp_entry = new_temp
-				counter += 1
-
-			if counter % iteration_amount == 0:
-				temp_list.insert(0, temp_entry)
-				counter = 0
-
-		first_round = True
-		temp_entry = None
-		for number in range(0, len(temp_list)):
-
-			if first_round is True:
-				temp_entry = temp_list[number]
-				first_round = False
-			else:
-				new_temp = self.get_concat_vertically(temp_entry, temp_list[number])
-				temp_entry = new_temp
-
-		return temp_entry
-
-	def concat_images(self, new_folder_path, request, tile_number):
-		"""
-		Combines nine tile images into one.
-		:param new_folder_path: The directory containing the tile images
-		:param request: The number of the request
-		:param tile_number: The lat/long identification of this tile
-		:return: Nothing
-		"""
-
-		up_left = Image.open(next(new_folder_path.glob("1_*")))
-		up_center = Image.open(next(new_folder_path.glob("2_*")))
-		up_right = Image.open(next(new_folder_path.glob("3_*")))
-		center_left = Image.open(next(new_folder_path.glob("4_*")))
-		center_center = Image.open(next(new_folder_path.glob("5_*")))
-		center_right = Image.open(next(new_folder_path.glob("6_*")))
-		down_left = Image.open(next(new_folder_path.glob("7_*")))
-		down_center = Image.open(next(new_folder_path.glob("8_*")))
-		down_right = Image.open(next(new_folder_path.glob("9_*")))
 		images = [
 			up_left,
 			up_center,
@@ -125,9 +48,17 @@ class ImageUtil:
 			down_center,
 			down_right
 		]
-		result = self.concat_image_grid(3, 3, images)
-		temp_name = "request_" + str(request) + "_tile_" + tile_number
-		result.save(Path.joinpath(new_folder_path, "concat_image_" + temp_name + ".png"))
+		return self.concat_image_grid(3, 3, images)
+
+	# pylint: disable=C0200
+	def combine_images_list(self, image_list, iteration_amount):
+		"""
+		The method to concatenate all the images from a list
+		:param image_list: the list with all the images to be concatenated
+		:param iteration_amount: how many images are in one row of the concatenated image
+		:return: the concatenated image
+		"""
+		return self.concat_image_grid(self,iteration_amount,int(len(image_list)/iteration_amount),image_list)
 
 	# pylint: disable=E1120
 	def concat_image_grid(self, width, height, images):
