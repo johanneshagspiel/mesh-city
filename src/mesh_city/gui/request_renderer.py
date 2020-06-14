3bd818512840537996f2529e10951bf35a95de32
tbvanderwoude <t.b.vanderwoude@student.tudelft.nl>
2020-06-14 12:01:33 +0200

    Implemented reduced resolution rendering of requests

--- src/mesh_city/gui/request_renderer.py
+++ src/mesh_city/gui/request_renderer.py
@@ -2,9 +2,9 @@
 See :class:`.RequestRenderer`
 """
 import csv
-import geopandas as gpd
 from typing import List
 
+import geopandas as gpd
 from PIL import Image, ImageDraw
 
 from mesh_city.request.buildings_layer import BuildingsLayer
@@ -20,7 +20,7 @@ class RequestRenderer:
 	"""
 
 	@staticmethod
-	def render_request(request: Request, layer_mask: List[bool]) -> Image:
+	def render_request(request: Request, layer_mask: List[bool],scaling=4) -> Image:
 		"""
 		Composites a rendering of a selected number of layers of a request.
 		:param request: The request to create an image for
@@ -28,7 +28,7 @@ class RequestRenderer:
 		:return: An image representation of the layer.
 		"""
 		base_image = Image.new(
-			'RGBA', (request.num_of_horizontal_images * 1024, request.num_of_vertical_images * 1024),
+			'RGBA', (round(request.num_of_horizontal_images * 1024/scaling), round(request.num_of_vertical_images * 1024/scaling)),
 			(255, 255, 255, 0)
 		)
 		result_image = base_image
@@ -36,12 +36,12 @@ class RequestRenderer:
 			if mask:
 				result_image = Image.alpha_composite(
 					im1=result_image,
-					im2=RequestRenderer.create_image_from_layer(request=request, layer_index=index)
+					im2=RequestRenderer.create_image_from_layer(request=request, layer_index=index,scaling=scaling)
 				)
 		return result_image
 
 	@staticmethod
-	def create_image_from_layer(request: Request, layer_index: int) -> Image:
+	def create_image_from_layer(request: Request, layer_index: int, scaling=4) -> Image:
 		"""
 		Creates an image from a specific layer of a request.
 		:param request: The request to create an image for
@@ -54,7 +54,7 @@ class RequestRenderer:
 			overlays = []
 			tree_overlay = Image.new(
 				'RGBA',
-				(request.num_of_horizontal_images * 1024, request.num_of_vertical_images * 1024),
+				(round(request.num_of_horizontal_images * 1024/scaling), round(request.num_of_vertical_images * 1024/scaling)),
 				(255, 255, 255, 0)
 			)
 			draw = ImageDraw.Draw(tree_overlay)
@@ -63,7 +63,7 @@ class RequestRenderer:
 				for (index, row) in enumerate(csv_reader):
 					if len(row) > 0 and index > 0:
 						draw.rectangle(
-							xy=((float(row[1]), float(row[2])), (float(row[3]), float(row[4]))),
+							xy=((float(row[1])/scaling, float(row[2])/scaling), (float(row[3])/scaling, float(row[4])/scaling)),
 							outline="red"
 						)
 				overlays.append(tree_overlay)
@@ -72,7 +72,9 @@ class RequestRenderer:
 			tiles = layer.tiles
 			images = []
 			for tile in tiles:
-				images.append(Image.open(tile.path).convert("RGBA"))
+				large_image = Image.open(tile.path).convert("RGBA")
+				width, height = large_image.size
+				images.append(large_image.resize((round(width/scaling),round(height/scaling))))
 			concat_image = ImageUtil.concat_image_grid(
 				width=request.num_of_horizontal_images,
 				height=request.num_of_vertical_images,
@@ -81,15 +83,19 @@ class RequestRenderer:
 			return concat_image
 
 		if isinstance(layer, BuildingsLayer):
-
 			building_dataframe = gpd.read_file(layer.detections_path)
+			building_dataframe.geometry = building_dataframe.geometry.scale(
+				xfact=1/scaling, yfact=1/scaling, zfact=1.0, origin=(0, 0)
+			)
 			building_overlay = Image.new(
 				'RGBA',
-				(request.num_of_horizontal_images * 1024, request.num_of_vertical_images * 1024),
+				(round(request.num_of_horizontal_images * 1024/scaling), round(request.num_of_vertical_images * 1024/scaling)),
 				(255, 255, 255, 0)
 			)
 			draw = ImageDraw.Draw(building_overlay)
 			for polygon in building_dataframe["geometry"]:
-				draw.polygon(xy=list(zip(*polygon.exterior.coords.xy)),outline="red")
+				draw.polygon(
+					xy=list(zip(*polygon.exterior.coords.xy)), fill=(255, 0, 0, 50), outline="red"
+				)
 			return building_overlay
 		raise ValueError("The overlay could not be created")
