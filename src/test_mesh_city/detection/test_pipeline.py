66c6d0fde66c6a41a74c41c80d0ec5df93a4f5a3
tbvanderwoude <t.b.vanderwoude@student.tudelft.nl>
2020-06-16 23:02:46 +0200

    Fixed broken pipeline test and improved tests

--- src/test_mesh_city/detection/test_pipeline.py
+++ src/test_mesh_city/detection/test_pipeline.py
@@ -1,31 +1,47 @@
 # pylint: disable=C0114,E1141,R0201,W0621,missing-class-docstring,missing-function-docstring
 
 import unittest
+import pandas as pd
+import geopandas as gpd
 from pathlib import Path
 
 from mesh_city.detection.detection_pipeline import DetectionPipeline, DetectionType
+from mesh_city.request.layers.buildings_layer import BuildingsLayer
+from mesh_city.request.layers.cars_layer import CarsLayer
+from mesh_city.request.layers.trees_layer import TreesLayer
 from mesh_city.request.request_manager import RequestManager
 from mesh_city.util.file_handler import FileHandler
 
 
 class TestPipeline(unittest.TestCase):
+	def setUp(self) -> None:
+		self.file_handler = FileHandler(root=Path(__file__).parents[1])
+		self.request_manager = RequestManager(self.file_handler.folder_overview["image_path"])
+		self.request_manager.load_data()
 
 	def test_tree_detection(self):
-		file_handler = FileHandler()
-		request_manager = RequestManager(file_handler.folder_overview["image_path"])
-		request_manager.load_data()
 		pipeline = DetectionPipeline(
-			FileHandler(), request_manager, detections_to_run=[DetectionType.TREES]
+			FileHandler(), self.request_manager, detections_to_run=[DetectionType.TREES]
 		)
-		request = request_manager.get_request_by_id(0)
+		request = self.request_manager.get_request_by_id(0)
 		pipeline.process(request)
+		tree_dataframe = pd.read_csv(request.get_layer_of_type(TreesLayer).detections_path)
+		self.assertGreaterEqual(len(tree_dataframe),10)
+
+	def test_car_detection(self):
+		pipeline = DetectionPipeline(
+			self.file_handler, self.request_manager, detections_to_run=[DetectionType.CARS]
+		)
+		request = self.request_manager.get_request_by_id(0)
+		pipeline.process(request)
+		car_dataframe = pd.read_csv(request.get_layer_of_type(CarsLayer).detections_path)
+		self.assertGreaterEqual(len(car_dataframe),5)
 
 	def test_building_detection(self):
-		file_handler = FileHandler(root=Path(__file__).parents[1])
-		request_manager = RequestManager(file_handler.folder_overview["image_path"])
-		request_manager.load_data()
 		pipeline = DetectionPipeline(
-			file_handler, request_manager, detections_to_run=[DetectionType.BUILDINGS]
+			self.file_handler, self.request_manager, detections_to_run=[DetectionType.BUILDINGS]
 		)
-		request = request_manager.get_request_by_id(0)
+		request = self.request_manager.get_request_by_id(0)
 		pipeline.process(request)
+		buildings_geodf = gpd.read_file(request.get_layer_of_type(BuildingsLayer).detections_path)
+		self.assertTrue(len(buildings_geodf)<20 and len(buildings_geodf)>3)
